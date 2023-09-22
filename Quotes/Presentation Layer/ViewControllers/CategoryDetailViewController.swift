227e9d9f547e59f8d03334265957813577f5e1e8
Krostal <krostal1991@gmail.com>
2023-09-22 16:21:02 +0700

    fixed height of navigation bar

---
+++ Quotes/Presentation Layer/ViewControllers/CategoryDetailViewController.swift
@@ -0,0 +1,47 @@
+
+import UIKit
+
+class CategoryDetailViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    let interactor: QuoteInteractorProtocol = QuoteInteractor(realmService: RealmService(), dataService: DataService())
+        
+    var category: String = ""
+    var quotes: [Quote] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchQuotesAndUpdateTable()
+        navigationItem.title = category
+    }
+    
+    private func fetchQuotesAndUpdateTable() {
+        quotes = interactor.getQuotesForCategory(category)
+        tableView.reloadData()
+    }
+    
+}
+
+
+extension CategoryDetailViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        quotes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryDetailCell", for: indexPath) as? CategoryDetailTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        
+        let quote = quotes[indexPath.row]
+        
+        cell.configure(with: quote)
+        
+        return cell
+    }
+    
+    
+}
